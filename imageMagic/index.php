be19b8a60e303c7d4c0ea7c35a7ff1f9bf9297fd
mariakhachatryann <mariakhachatryan23@gmail.com>
2024-03-29 13:46:21 +0400

    removed some files

--- imageMagic/index.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <h2>Upload Image</h2>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="image" accept="image/*">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
-</div>
